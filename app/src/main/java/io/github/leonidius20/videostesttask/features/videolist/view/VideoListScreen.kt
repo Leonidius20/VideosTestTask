a3e9e2bbff036d32dced60e626293d6c50b8ab4f
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-10-01 17:31:45 +0300

    videos list is now shown

--- app/src/main/java/io/github/leonidius20/videostesttask/features/videolist/view/VideoListScreen.kt
+++ app/src/main/java/io/github/leonidius20/videostesttask/features/videolist/view/VideoListScreen.kt
@@ -1,6 +1,13 @@
 package io.github.leonidius20.videostesttask.features.videolist.view
 
+import android.icu.text.CaseMap.Title
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -42,10 +49,33 @@ private fun LoadedVideosListScreen(
     data: List<Video>,
     isRefreshInProgress: Boolean,
     refreshErrorMessage: String?,
+    // onclick
 ) {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Text(
-            text = data.size.toString()
-        )
+        LazyColumn(modifier = Modifier.padding(innerPadding)) {
+            items(items = data, key = { video -> video.videoUrl }) { video ->
+                VideoListItem(
+                    modifier = Modifier.clickable { /*todo*/ },
+                    video = video,
+                )
+            }
+        }
     }
+}
+
+@Composable
+private fun VideoListItem(
+    modifier: Modifier = Modifier,
+    video: Video,
+) {
+    ListItem(
+        modifier = modifier,
+        headlineContent = { Text(text = video.title) },
+        supportingContent = {
+            Column {
+                Text(text = video.subtitle)
+                Text(text = video.description)
+            }
+        }
+    )
 }
