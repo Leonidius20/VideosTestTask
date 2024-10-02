63ba016fc0bf2ab22180034d7ede6fb5c3ae13b1
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-10-02 18:14:58 +0300

    fixed the playback and playlist

--- app/src/main/java/io/github/leonidius20/videostesttask/features/player/view/PlayerScreen.kt
+++ app/src/main/java/io/github/leonidius20/videostesttask/features/player/view/PlayerScreen.kt
@@ -1,11 +1,13 @@
 package io.github.leonidius20.videostesttask.features.player.view
 
+import android.util.Log
 import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -13,7 +15,6 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.media3.common.MediaItem
-import androidx.media3.common.MediaMetadata
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultHttpDataSource
@@ -32,13 +33,36 @@ fun PlayerScreen() {
     val context = LocalContext.current
 
     val player = remember {
-        ExoPlayer.Builder(context)
-            .build()
-            .apply {
-                setMediaSource(ProgressiveMediaSource.Factory(DefaultHttpDataSource.Factory()).createMediaSource(
-                    MediaItem.fromUri(viewModel.initialVideoUrl)
-                ))
+            ExoPlayer.Builder(context)
+                .build()
+    }
+
+    // each time videos list changes, we re-set playlist
+    LaunchedEffect(key1 = state.value) {
+        val videos = state.value.videos
+        player.setMediaSources(
+
+            videos.map { video ->
+                // Log.d("video", "video ${video.name}")
+                ProgressiveMediaSource.Factory(DefaultHttpDataSource.Factory())
+                    .createMediaSource(
+                        MediaItem.fromUri(video.url)
+                    )
             }
+
+        )
+        player.prepare()
+
+        // seeking to the video that should be playing
+        Log.d("video", "before seeking")
+        if (videos.isNotEmpty()) {
+            Log.d("vieo", "playlist size ${player.mediaItemCount}")
+            player.seekTo(
+                videos.indexOfFirst { it.isPlaying }, 0
+            )
+        }
+
+        player.play()
     }
 
     Scaffold { innerPadding ->
@@ -50,21 +74,7 @@ fun PlayerScreen() {
                         setShowBuffering(PlayerView.SHOW_BUFFERING_WHEN_PLAYING)
                         setPlayer(player)
 
-
-                        //player.setMediaItems(state.value.videos.map { video ->
-                        //    MediaItem.fromUri(video.url)
-                            /*MediaItem.Builder()
-                                .setUri(video.url)
-                                .setMediaId(video.url)
-                                .setMediaMetadata(
-                                    MediaMetadata.Builder()
-                                        .setDisplayTitle(video.name)
-                                        .build()
-                                )
-                                .build()*/
-                        //})
-
-                        player.addListener(object : Player.Listener {
+                        /*player.addListener(object : Player.Listener {
 
                             override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
                                 mediaItem?.run {
@@ -72,13 +82,13 @@ fun PlayerScreen() {
                                 }
                             }
 
-                        })
+                        })*/
 
-                        player.prepare()
+                        //player.prepare()
 
-                        player.play()
+                        // player.play()
                     }
-                }
+                },
             )
         }
     }
